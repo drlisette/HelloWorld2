cddb6a4a7fde5ebd8228208f2c99986181962e77
drlisette <ldr31795@126.com>
2017-10-22 20:13:26 +0800

    Initial commit

---
+++ app/src/main/java/com/a091517/ldr/helloworld/FirstTry.java
@@ -0,0 +1,13 @@
+package com.a091517.ldr.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstTry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_try);
+    }
+}
